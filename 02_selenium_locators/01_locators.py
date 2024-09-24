3cb1b6f47bca5e5d4d7b3377689272e2caa1e1d5
Sahulinkan7 <sahulinkan7@gmail.com>
2024-09-24 11:51:24 +0530

    updated 01_locators

--- 02_selenium_locators/01_locators.py
+++ 02_selenium_locators/01_locators.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-
+import os
 
 driver=webdriver.Chrome()
 driver.maximize_window()
@@ -9,20 +9,21 @@ driver.maximize_window()
 driver.get("https://www.saucedemo.com/")
 
 ## find element by CLASS
-driver.find_element(By.CLASS_NAME,"login_container").screenshot("01_no_credential_login.png")
+os.makedirs("screenshots",exist_ok=True)
+driver.find_element(By.CLASS_NAME,"login_container").screenshot("screenshots/01_locators_01_nocredential_login.png")
 ## find element by ID
 
 driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.ID,"password").send_keys("secret_sauce")
 
 ## find element by CLASS
-driver.find_element(By.CLASS_NAME,"login_container").screenshot("02_with_credential_login.png")
+driver.find_element(By.CLASS_NAME,"login_container").screenshot("screenshots/01_locators_02_withcredential_login.png")
 
 ## find element by ID
 driver.find_element(By.ID,"login-button").click()
 print(driver.current_url)
 time.sleep(2)
-driver.find_element(By.CLASS_NAME,"inventory_container").screenshot("03_after_login.png")
+driver.find_element(By.CLASS_NAME,"inventory_container").screenshot("screenshots/01_locators_03_afterlogin.png")
 
 ## By CLASS NAME
 products_quantity=driver.find_elements(By.CLASS_NAME,"inventory_item")
