9498451d4b610f1c34dc503954c3b7a2947ced6a
Sahulinkan7 <sahulinkan7@gmail.com>
2024-09-27 21:57:04 +0530

    pytest tests

--- 05_data_driven_testing/02_write_excel.py
+++ 05_data_driven_testing/02_write_excel.py
@@ -10,3 +10,9 @@ workbook.save("testdata.xlsx")
 filepath="./testdata.xlsx"
 
 workbook=openpyxl.load_workbook(filepath)
+
+## saving data 
+for r in range(1,6):
+    for c in range(1,5):
+        workbook['Data'].cell(r,c).value=f"value[{r}{c}]"
+workbook.save("testdata.xlsx")
---
+++ 06_pytest/modules/test_first.py
@@ -0,0 +1,8 @@
+import pytest 
+
+class TestClass:
+    def test_method1(self):
+        print("test method 1")
+        assert 1==1
+        
+        
