5b96571bc8c0d31f713361949b11da91a804373d
Sahulinkan7 <sahulinkan7@gmail.com>
2024-09-28 21:23:58 +0530

    grouping test methods

--- 06_pytest/modules/test_first.py
+++ 06_pytest/modules/test_first.py
@@ -1,8 +1,18 @@
 import pytest 
 
 class TestClass:
+    
     def test_method1(self):
         print("test method 1")
         assert 1==1
         
+    @pytest.mark.sanity
+    def test_method2(self):
+        print("test method 2")
+        assert 1==1
+        
+    @pytest.mark.regression
+    def test_method3(self):
+        print("test method 3")
+        assert 1==1
         
